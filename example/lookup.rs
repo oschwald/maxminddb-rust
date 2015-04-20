9d7d8c895d19e88121e93dd15008cf277d5d759a
Gregory Oschwald <goschwald@maxmind.com>
2015-04-19 17:03:51 -0700

    Clean up old example

--- example/lookup.rs
+++ example/lookup.rs
@@ -9,8 +9,6 @@ use maxminddb::geoip2;
 fn main() {
     let reader = maxminddb::Reader::open("/usr/local/share/GeoIP/GeoIP2-City.mmdb").unwrap();
     let ip: SocketAddr = FromStr::from_str("128.101.101.101:0").unwrap();
-    let decoded_object: geoip2::City = r.lookup(ip).unwrap();
-    print!("{:?}\n", decoded_object);
-    print!("{:?}\n",decoded_object.country);
-
+    let city: geoip2::City = reader.lookup(ip).unwrap();
+    print!("{:?}\n", city);
 }
