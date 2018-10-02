74081105875eafd9884a00979b2bcf1ab606fab7
Travis CI User <travis@example.org>
2018-10-02 18:32:55 +0000

    Documentation for oschwald/maxminddb-rust@f0ff7fe

---
+++ aliases.js
@@ -0,0 +1,10 @@
+var ALIASES = {};
+ALIASES['cfg_if'] = {};
+ALIASES['log'] = {};
+ALIASES['maxminddb'] = {};
+ALIASES['proc_macro2'] = {};
+ALIASES['quote'] = {};
+ALIASES['serde'] = {};
+ALIASES['serde_derive'] = {};
+ALIASES['syn'] = {};
+ALIASES['unicode_xid'] = {};
---
+++ implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["syn"] = [{text:"impl&lt;'c, 'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"syn/parse/struct.StepCursor.html\" title=\"struct syn::parse::StepCursor\">StepCursor</a>&lt;'c, 'a&gt;",synthetic:false,types:["syn::parse::StepCursor"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
