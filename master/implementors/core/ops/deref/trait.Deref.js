df5b2181eea160853944a9589290a1a3c9d12c33
Travis CI User <travis@example.org>
2018-11-13 01:45:12 +0000

    Automatic Travis documentation build

--- master/implementors/core/ops/deref/trait.Deref.js
+++ master/implementors/core/ops/deref/trait.Deref.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["maxminddb"] = [{text:"impl&lt;'data&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"type\" href=\"maxminddb/type.OwnedReader.html\" title=\"type maxminddb::OwnedReader\">OwnedReader</a>&lt;'data&gt;",synthetic:false,types:["maxminddb::OwnedReader"]},];
 implementors["syn"] = [{text:"impl&lt;'c, 'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"syn/parse/struct.StepCursor.html\" title=\"struct syn::parse::StepCursor\">StepCursor</a>&lt;'c, 'a&gt;",synthetic:false,types:["syn::parse::StepCursor"]},];
 
             if (window.register_implementors) {
