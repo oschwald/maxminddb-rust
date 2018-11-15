f8139de5d6b7fe7713c9a0a67f6b44da8e2e68be
Travis CI User <travis@example.org>
2018-11-15 07:12:33 +0000

    Automatic Travis documentation build

--- master/aliases.js
+++ master/aliases.js
@@ -40,6 +40,7 @@ ALIASES['maxminddb'] = {};
 ALIASES['maxminddb'] = {};
 ALIASES['maxminddb'] = {};
 ALIASES['maxminddb'] = {};
+ALIASES['maxminddb'] = {};
 ALIASES['proc_macro2'] = {};
 ALIASES['proc_macro2'] = {};
 ALIASES['proc_macro2'] = {};
