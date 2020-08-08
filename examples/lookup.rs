da2b72991526510ecf18b72e1fec39b4ab5f1e67
Gregory Oschwald <goschwald@maxmind.com>
2020-08-08 12:27:08 -0700

    Run cargo fmt

--- examples/lookup.rs
+++ examples/lookup.rs
@@ -2,11 +2,18 @@ use std::net::IpAddr;
 
 use maxminddb::geoip2;
 
-fn main() -> Result<(), String>{
+fn main() -> Result<(), String> {
     let mut args = std::env::args().skip(1);
-    let reader =
-        maxminddb::Reader::open_readfile(args.next().ok_or_else(|| "First argument must be the path to the IP database")?).unwrap();
-    let ip: IpAddr = args.next().ok_or_else(|| "Second argument must be the IP address, like 128.101.101.101")?.parse().unwrap();
+    let reader = maxminddb::Reader::open_readfile(
+        args.next()
+            .ok_or_else(|| "First argument must be the path to the IP database")?,
+    )
+    .unwrap();
+    let ip: IpAddr = args
+        .next()
+        .ok_or_else(|| "Second argument must be the IP address, like 128.101.101.101")?
+        .parse()
+        .unwrap();
     let city: geoip2::City = reader.lookup(ip).unwrap();
     println!("{:#?}", city);
     Ok(())
