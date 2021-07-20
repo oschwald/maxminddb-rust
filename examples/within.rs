b629400ec826f1de9dfc87278f22087281f388b6
Ross McFarland <rwmcfa1@gmail.com>
2021-07-20 11:11:40 -0700

    within example

---
+++ examples/within.rs
@@ -0,0 +1,28 @@
+use ipnetwork::IpNetwork;
+
+use maxminddb::geoip2;
+use maxminddb::Within;
+
+fn main() -> Result<(), String> {
+    let mut args = std::env::args().skip(1);
+    let reader = maxminddb::Reader::open_readfile(
+        args.next()
+            .ok_or("First argument must be the path to the IP database")?,
+    )
+    .unwrap();
+    let cidr: String = args
+        .next()
+        .ok_or("Second argument must be the IP address and mask in CIDR notation")?
+        .parse()
+        .unwrap();
+    let ip_net = if cidr.contains(":") {
+        IpNetwork::V6(cidr.parse().unwrap())
+    } else {
+        IpNetwork::V4(cidr.parse().unwrap())
+    };
+    let within: Within<_, geoip2::City> = reader.within(ip_net).unwrap();
+    for item in within {
+        println!("item={:#?}", item);
+    }
+    Ok(())
+}
