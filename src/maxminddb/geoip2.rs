886786d46369b9d3ac75ce918351273ebdc98361
Gregory Oschwald <goschwald@maxmind.com>
2022-03-23 09:54:13 -0700

    Added Enterprise struct as well as missing fields for other DB types

--- src/maxminddb/geoip2.rs
+++ src/maxminddb/geoip2.rs
@@ -26,12 +26,29 @@ pub struct City<'a> {
     pub traits: Option<model::Traits>,
 }
 
+/// GeoIP2 Enterprise record
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub struct Enterprise<'a> {
+    pub city: Option<enterprise::City<'a>>,
+    #[serde(borrow)]
+    pub continent: Option<enterprise::Continent<'a>>,
+    pub country: Option<enterprise::Country<'a>>,
+    pub location: Option<enterprise::Location<'a>>,
+    pub postal: Option<enterprise::Postal<'a>>,
+    pub registered_country: Option<enterprise::Country<'a>>,
+    pub represented_country: Option<enterprise::RepresentedCountry<'a>>,
+    pub subdivisions: Option<Vec<enterprise::Subdivision<'a>>>,
+    pub traits: Option<enterprise::Traits<'a>>,
+}
+
 /// GeoIP2 ISP record
 #[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct Isp<'a> {
     pub autonomous_system_number: Option<u32>,
     pub autonomous_system_organization: Option<&'a str>,
     pub isp: Option<&'a str>,
+    pub mobile_country_code: Option<&'a str>,
+    pub mobile_network_code: Option<&'a str>,
     pub organization: Option<&'a str>,
 }
 
@@ -48,6 +65,7 @@ pub struct AnonymousIp {
     pub is_anonymous_vpn: Option<bool>,
     pub is_hosting_provider: Option<bool>,
     pub is_public_proxy: Option<bool>,
+    pub is_residential_proxy: Option<bool>,
     pub is_tor_exit_node: Option<bool>,
 }
 
@@ -71,6 +89,7 @@ pub struct Asn<'a> {
     pub autonomous_system_organization: Option<&'a str>,
 }
 
+/// Country and City model structs
 pub mod model {
     use serde::{Deserialize, Serialize};
     use std::collections::BTreeMap;
@@ -99,6 +118,7 @@ pub mod model {
 
     #[derive(Deserialize, Serialize, Clone, Debug)]
     pub struct Location<'a> {
+        pub accuracy_radius: Option<u16>,
         pub latitude: Option<f64>,
         pub longitude: Option<f64>,
         pub metro_code: Option<u16>,
@@ -113,8 +133,11 @@ pub mod model {
     #[derive(Deserialize, Serialize, Clone, Debug)]
     pub struct RepresentedCountry<'a> {
         pub geoname_id: Option<u32>,
+        pub is_in_european_union: Option<bool>,
         pub iso_code: Option<&'a str>,
-        pub names: Option<BTreeMap<&'a str, &'a str>>, // pub type: Option<&'a str>,
+        pub names: Option<BTreeMap<&'a str, &'a str>>,
+        #[serde(rename = "type")]
+        pub representation_type: Option<&'a str>,
     }
 
     #[derive(Deserialize, Serialize, Clone, Debug)]
@@ -128,5 +151,89 @@ pub mod model {
     pub struct Traits {
         pub is_anonymous_proxy: Option<bool>,
         pub is_satellite_provider: Option<bool>,
+    }
+}
+
+/// Enterprise model structs
+pub mod enterprise {
+    use serde::{Deserialize, Serialize};
+    use std::collections::BTreeMap;
+
+    #[derive(Deserialize, Serialize, Clone, Debug)]
+    pub struct City<'a> {
+        pub confidence: Option<u8>,
+        pub geoname_id: Option<u32>,
+        #[serde(borrow)]
+        pub names: Option<BTreeMap<&'a str, &'a str>>,
+    }
+
+    #[derive(Deserialize, Serialize, Clone, Debug)]
+    pub struct Continent<'a> {
+        pub code: Option<&'a str>,
+        pub geoname_id: Option<u32>,
+        pub names: Option<BTreeMap<&'a str, &'a str>>,
+    }
+
+    #[derive(Deserialize, Serialize, Clone, Debug)]
+    pub struct Country<'a> {
+        pub confidence: Option<u8>,
+        pub geoname_id: Option<u32>,
+        pub is_in_european_union: Option<bool>,
+        pub iso_code: Option<&'a str>,
+        pub names: Option<BTreeMap<&'a str, &'a str>>,
+    }
+
+    #[derive(Deserialize, Serialize, Clone, Debug)]
+    pub struct Location<'a> {
+        pub accuracy_radius: Option<u16>,
+        pub latitude: Option<f64>,
+        pub longitude: Option<f64>,
+        pub metro_code: Option<u16>,
+        pub time_zone: Option<&'a str>,
+    }
+
+    #[derive(Deserialize, Serialize, Clone, Debug)]
+    pub struct Postal<'a> {
+        pub confidence: Option<u8>,
+        pub code: Option<&'a str>,
+    }
+
+    #[derive(Deserialize, Serialize, Clone, Debug)]
+    pub struct RepresentedCountry<'a> {
+        pub geoname_id: Option<u32>,
+        pub is_in_european_union: Option<bool>,
+        pub iso_code: Option<&'a str>,
+        pub names: Option<BTreeMap<&'a str, &'a str>>,
+        #[serde(rename = "type")]
+        pub representation_type: Option<&'a str>,
+    }
+
+    #[derive(Deserialize, Serialize, Clone, Debug)]
+    pub struct Subdivision<'a> {
+        pub confidence: Option<u8>,
+        pub geoname_id: Option<u32>,
+        pub iso_code: Option<&'a str>,
+        pub names: Option<BTreeMap<&'a str, &'a str>>,
+    }
+
+    #[derive(Deserialize, Serialize, Clone, Debug)]
+    pub struct Traits<'a> {
+        pub autonomous_system_number: Option<u32>,
+        pub autonomous_system_organization: Option<&'a str>,
+        pub connection_type: Option<&'a str>,
+        pub domain: Option<&'a str>,
+        pub is_anonymous: Option<bool>,
+        pub is_anonymous_proxy: Option<bool>,
+        pub is_anonymous_vpn: Option<bool>,
+        pub is_hosting_provider: Option<bool>,
+        pub isp: Option<&'a str>,
+        pub is_public_proxy: Option<bool>,
+        pub is_residential_proxy: Option<bool>,
+        pub is_satellite_provider: Option<bool>,
+        pub is_tor_exit_node: Option<bool>,
+        pub mobile_country_code: Option<&'a str>,
+        pub mobile_network_code: Option<&'a str>,
+        pub organization: Option<&'a str>,
+        pub user_type: Option<&'a str>,
     }
 }
