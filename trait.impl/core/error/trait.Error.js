927129668763a7a21d11d8e3465723b5e3823baa
oschwald <oschwald@users.noreply.github.com>
2026-02-14 04:08:22 +0000

    deploy: 3a79c76ca84a555376675ed5c89ad7847afa3a14

--- trait.impl/core/error/trait.Error.js
+++ trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["maxminddb",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"maxminddb/enum.MaxMindDbError.html\" title=\"enum maxminddb::MaxMindDbError\">MaxMindDbError</a>"]]]]);
+    var implementors = Object.fromEntries([["maxminddb",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"maxminddb/enum.MaxMindDbError.html\" title=\"enum maxminddb::MaxMindDbError\">MaxMindDbError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
