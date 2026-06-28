adbd5f0157406fa4f7c5ef4338df458b90ea3374
oschwald <oschwald@users.noreply.github.com>
2026-06-28 23:04:27 +0000

    deploy: 7a44f85312e354d0578cdb1f207995ec41c13edb

--- trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++ trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["maxminddb",[["impl&lt;'de, S: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.95.0/std/primitive.u8.html\">u8</a>]&gt;&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"maxminddb/struct.Within.html\" title=\"struct maxminddb::Within\">Within</a>&lt;'de, S&gt;",0]]]]);
+    const implementors = Object.fromEntries([["maxminddb",[["impl&lt;'de, S: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.96.0/std/primitive.u8.html\">u8</a>]&gt;&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"maxminddb/struct.Within.html\" title=\"struct maxminddb::Within\">Within</a>&lt;'de, S&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
