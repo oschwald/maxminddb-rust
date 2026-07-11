e9b9afec9121719c0725b6fc37390241b66d6c08
Gregory Oschwald <oschwald@gmail.com>
2026-07-11 12:11:23 -0700

    Benchmark reader construction

--- benches/metadata.rs
+++ benches/metadata.rs
@@ -5,8 +5,14 @@ use criterion::{criterion_group, criterion_main, Criterion};
 const DB_FILE: &str = "GeoLite2-City.mmdb";
 
 pub fn metadata_benchmark(c: &mut Criterion) {
+    let database = std::fs::read(DB_FILE).unwrap();
     let reader = maxminddb::Reader::open_readfile(DB_FILE).unwrap();
 
+    c.bench_function("reader/from_source", |b| {
+        b.iter(|| {
+            black_box(maxminddb::Reader::from_source(black_box(database.as_slice())).unwrap())
+        })
+    });
     c.bench_function("metadata/build_time", |b| {
         b.iter(|| black_box(reader.metadata().build_time().unwrap()))
     });
