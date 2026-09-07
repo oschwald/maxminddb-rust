cb1848bc41e25466224bc6a24a7821c4de5dcda4
oschwald <oschwald@users.noreply.github.com>
2026-09-07 19:22:29 +0000

    deploy: fe128ab9bf78e71a162f73af4265b7ed54eebc2a

--- trait.impl/serde_core/de/trait.Error.js
+++ trait.impl/serde_core/de/trait.Error.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["maxminddb",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.229/serde_core/de/trait.Error.html\" title=\"trait serde_core::de::Error\">Error</a> for <a class=\"enum\" href=\"maxminddb/enum.MaxMindDbError.html\" title=\"enum maxminddb::MaxMindDbError\">MaxMindDbError</a>",0]]]]);
+    const implementors = Object.fromEntries([["maxminddb",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.229/serde_core/de/trait.Error.html\" title=\"trait serde_core::de::Error\">Error</a> for <a class=\"struct\" href=\"https://doc.rust-lang.org/1.98.1/alloc/boxed/struct.Box.html\" title=\"struct alloc::boxed::Box\">Box</a>&lt;<a class=\"enum\" href=\"maxminddb/enum.MaxMindDbError.html\" title=\"enum maxminddb::MaxMindDbError\">MaxMindDbError</a>&gt;",0],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.229/serde_core/de/trait.Error.html\" title=\"trait serde_core::de::Error\">Error</a> for <a class=\"enum\" href=\"maxminddb/enum.MaxMindDbError.html\" title=\"enum maxminddb::MaxMindDbError\">MaxMindDbError</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[294]}
+//{"start":59,"fragment_lengths":[713]}
